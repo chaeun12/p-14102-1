6e4b27ccc3c0fa28fcf0f8c869fccf6dc05e1f85
chaeun12 <145729167+chaeun12@users.noreply.github.com>
2025-12-03 15:37:09 +0900

    work

--- back/src/main/java/com/back/domain/post/post/controller/PostController.java
+++ back/src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -5,18 +5,12 @@ import com.back.domain.post.post.service.PostService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,35 +25,31 @@ public class PostController {
     }
 
 
-    @AllArgsConstructor
-    @Getter
-    @Setter
-    public static class ModifyForm {
-        @NotBlank(message = "01-title-제목을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
-        private String title;
-        @NotBlank(message = "03-content-내용을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
-        private String content;
+    record ModifyForm(
+            @NotBlank(message = "01-title-제목을 입력해주세요.")
+            @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
+            String title,
+            @NotBlank(message = "03-content-내용을 입력해주세요.")
+            @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
+            String content
+    ) {
     }
 
     @GetMapping("/posts/{id}/modify")
     @Transactional(readOnly = true)
     public String showModify(
             @PathVariable int id,
-            @ModelAttribute("form") ModifyForm form,
             Model model
     ) {
         Post post = postService.findById(id).get();
 
         model.addAttribute("post", post);
-        form.setTitle(post.getTitle());
-        form.setContent(post.getContent());
+        model.addAttribute("form", new ModifyForm(post.getTitle(), post.getContent()));
 
         return "post/post/modify";
     }
 
-    @PostMapping("/posts/{id}/modify")
+    @PutMapping("/posts/{id}/modify")
     @Transactional
     public String modify(
             @PathVariable int id,
@@ -74,21 +64,21 @@ public class PostController {
             return "post/post/modify";
         }
 
-        postService.modify(post, form.getTitle(), form.getContent());
+        postService.modify(post, form.title, form.content);
 
         return "redirect:/posts/" + post.getId();
     }
 
 
-    @AllArgsConstructor
-    @Getter
-    public static class WriteForm {
-        @NotBlank(message = "01-title-제목을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
-        private String title;
-        @NotBlank(message = "03-content-내용을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
-        private String content;
+    record WriteForm(
+            @NotBlank(message = "01-title-제목을 입력해주세요.")
+            @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
+            String title,
+            @NotBlank(message = "03-content-내용을 입력해주세요.")
+            @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
+            String content
+    ) {
+
     }
 
     @GetMapping("/posts/write")
@@ -100,14 +90,13 @@ public class PostController {
     @Transactional
     public String write(
             @ModelAttribute("form") @Valid WriteForm form,
-            BindingResult bindingResult,
-            Model model
+            BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) {
             return "post/post/write";
         }
 
-        Post post = postService.write(form.getTitle(), form.getContent());
+        Post post = postService.write(form.title, form.content);
 
         return "redirect:/posts/" + post.getId();
     }
--- back/src/main/java/com/back/domain/post/postComment/controller/PostCommentController.java
+++ back/src/main/java/com/back/domain/post/postComment/controller/PostCommentController.java
@@ -2,28 +2,70 @@ package com.back.domain.post.postComment.controller;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.domain.post.postComment.entity.PostComment;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class PostCommentController {
     private final PostService postService;
 
-    @AllArgsConstructor
-    @Getter
-    public static class WriteForm {
-        @NotBlank
-        @Size(min = 2, max = 100)
-        private String content;
+
+    record ModifyForm(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ) {
+
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}/modify")
+    @Transactional(readOnly = true)
+    public String showModify(
+            @PathVariable int postId,
+            @PathVariable int id,
+            Model model
+    ) {
+        Post post = postService.findById(postId).get();
+        PostComment postComment = post.findCommentById(id).get();
+
+        model.addAttribute("post", post);
+        model.addAttribute("postComment", postComment);
+
+        return "post/postComment/modify";
+    }
+
+    @PutMapping("/posts/{postId}/comments/{id}/modify")
+    @Transactional
+    public String modify(
+            @PathVariable int postId,
+            @PathVariable int id,
+            @Valid ModifyForm modifyForm
+    ) {
+        Post post = postService.findById(postId).get();
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.modifyComment(postComment, modifyForm.content);
+
+        return "redirect:/posts/" + postId;
+    }
+
+
+    record WriteForm(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ) {
     }
 
     @PostMapping("/posts/{postId}/comments/write")
@@ -34,7 +76,22 @@ public class PostCommentController {
     ) {
         Post post = postService.findById(postId).get();
 
-        postService.writeComment(post, writeForm.getContent());
+        postService.writeComment(post, writeForm.content);
+
+        return "redirect:/posts/" + postId;
+    }
+
+
+    @GetMapping("/posts/{postId}/comments/{id}/delete")
+    @Transactional
+    public String delete(
+            @PathVariable int postId,
+            @PathVariable int id
+    ) {
+        Post post = postService.findById(postId).get();
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.deleteComment(post, postComment);
 
         return "redirect:/posts/" + postId;
     }
